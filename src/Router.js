a0d3885ad3b6d01f9c6a0bb11b2ea8b5f018bfc9
Danie Ferreira <daniel.ferreira@imqs.co.za>
2018-01-29 11:12:46 +0200

    Remove stacknavigator animation to speed up switching of screens

--- src/Router.js
+++ src/Router.js
@@ -5,12 +5,24 @@ import ParentEditChild from './components/Parent/EditChild';
 import LoginForm from './components/Auth/LoginForm';
 import LoadingScreen from './components/Auth/LoadingScreen';
 
+const routerSettings = {
+  initialRouteName: 'LoadingScreen',
+  animationEnabled: false,
+  lazy: true,
+  // headerMode: 'none',
+  transitionConfig: () => ({ // This will remove animations, but make navigtion a lot faster
+    transitionSpec: {
+      duration: 0,
+    },
+  }),
+};
+
 const Router = StackNavigator({
   LoadingScreen: { screen: LoadingScreen },
   Login: { screen: LoginForm },
   ParentDashboard: { screen: ParentDashboard },
   ParentActivities: { screen: ParentActivities },
   ParentEditChild: { screen: ParentEditChild }
-});
+}, routerSettings);
 
 export default Router;
