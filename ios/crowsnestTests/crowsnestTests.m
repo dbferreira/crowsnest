7fc634448ea7683750feea3aa0baeb1df65377c7
Danie Ferreira <danie.ferreira1@gmail.com>
2018-04-20 15:39:52 +0200

    iOS fixes

--- ios/crowsnestTests/crowsnestTests.m
+++ ios/crowsnestTests/crowsnestTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface crowsnestTests : XCTestCase
+@interface CrowsNestTests : XCTestCase
 
 @end
 
-@implementation crowsnestTests
+@implementation CrowsNestTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
